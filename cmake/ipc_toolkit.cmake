d44215e740a574a4c8fa2e369bf039eac4e5f207
zhenjaychen(ZhenChen) <zhenjaychen@tencent.com>
2022-05-27 16:36:06 -0700

    fixed the cmake issue

--- cmake/ipc_toolkit.cmake
+++ cmake/ipc_toolkit.cmake
@@ -1,8 +1,8 @@
 include(FetchContent)
 FetchContent_Declare(
-    ipc_toolkit
+    ipc-toolkit
     GIT_REPOSITORY https://github.com/ipc-sim/ipc-toolkit.git
     GIT_TAG "e01609eab1d06c30164d8d6b486f294535a088de"
     GIT_SHALLOW TRUE
 )
-FetchContent_MakeAvailable(ipc_toolkit)
+FetchContent_MakeAvailable(ipc-toolkit)
