7d17fc32a646393e16ce6c0de0c90de4bbe23c46
Zhen Chen <zchen96@Zhens-MacBook-Pro.local>
2022-05-27 16:54:36 -0700

    update the ipc-toolkit

--- cmake/ipc_toolkit.cmake
+++ cmake/ipc_toolkit.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 FetchContent_Declare(
     ipc-toolkit
     GIT_REPOSITORY https://github.com/ipc-sim/ipc-toolkit.git
-    GIT_TAG "e01609eab1d06c30164d8d6b486f294535a088de"
+    GIT_TAG "913f3886a7c1c41e94ef6dcc2bda6215da92edbf"
     GIT_SHALLOW TRUE
 )
 FetchContent_MakeAvailable(ipc-toolkit)
